df4140d87bb0e3c5d939e507c33e0f803fe8e091
artur842 <artur842@icloud.com>
2020-03-12 21:01:29 +0000

    i may have finished the 2d arrays

--- pyWork/test.py
+++ pyWork/test.py
@@ -1,18 +1,22 @@
 uName = input("please write your name: ")
-day = [["monday", 0], ["tuesday", 0], ["wednesday", 0], ["thursday", 0], ["friday", 0], ["saturday", 0], ["sunday", 0]]
-uHours = []
+day = [["monday ", 0],
+       ["tuesday ", 0],
+       ["wednesday ", 0],
+       ["thursday ", 0],
+       ["friday ", 0],
+       ["saturday ", 0],
+       ["sunday ", 0]]
 avg = 0
 
 # ask section
-for n in day:
-    hWatching = input("how many hours were you watching tv on " + n)
-    # print (hWatching," on " , n)
-    avg += int(hWatching)
-    uHours.append(hWatching)
+for n in range(len(day)):
+    hWatching = int(input("how many hours were you watching tv on " + str(day[n][0])))
+    print(hWatching, " on ", day[n][0])
+    avg += hWatching
+    day[n][1] = hWatching
 avg /= len(day)  # len() finds the length of a variable
 
 # print section
 for i in range(len(day)):
-    print(i)
-    print(uHours[i], " on ", day[i])
+    print(day[i][1], "on ", day[i][0])
 print(avg, " is your average.")
