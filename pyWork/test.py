7f4b4b2ae2e03bdc7388c3f4e6cf97bede3f16f7
areon546 <57325743+areon546@users.noreply.github.com>
2020-04-13 18:35:46 +0100

    file intergration partial success, however progress; now need to figure out how to edit the files from the input if possible

--- pyWork/test.py
+++ pyWork/test.py
@@ -5,24 +5,26 @@ with open('testDays.txt', 'r') as f:
     day = []
 
     for line in f:
-        day = [[str(n) for n in line.split()] for line in f]
-    print(day)
+        number_strings = line.split()
+
+        day.append(number_strings)
+
+print(day)
 
 numb = len(day)
 avg = 0
 
 # ask section
 for n in range(0, numb):
-    # todo: need to make a new item in the 2d array without accessing it directly through editing the code
 
-    hWatching = int(input("how many hours were you watching tv on " + day[n][0]))
+    hWatching = int(input("How many hours were you watching tv on " + day[n][0]))
     print(hWatching, " on ", day[n][0])
     avg += hWatching
     day[n][1] = hWatching
 avg /= len(day)  # len() finds the length of a variable
-'''
+
 # print section
 for i in range(len(day)):
     print(day[i][1], "on ", day[i][0])
 print(avg, " is your average.")
-'''
+
--- pyWork/test3.py
+++ pyWork/test3.py
@@ -1,77 +1,46 @@
-# attempt at making each new line a secondary array
 
-"""
-f = open('Days.txt', 'r+')  # opens the file 'days'
+'''
+# THIS FIRST LINE WILL OPEN THE TEXT FILE WITH READ PERMISSION
+with open('Days.txt', 'r') as f:
+    # THIS LINE CREATES AN EMPTY ARRAY CALLED 'DAY'
+    day = []
 
-numb = len(open('Days.txt').readlines())    # counts the amount of lines in 'days'
-print(numb)                                 # prints the result from L.4
-
-day = []
-"""
-# todo make the test2 example create a new secondary array for each line
-
-"""
-testsite_array = []
-with open('topsites.txt') as my_file:
-    for line in my_file:
-        testsite_array.append(line)
-This is possible because Python allows you to iterate over the file directly.
-
-Alternatively, the more straightforward method, using f.readlines():
-
-with open('topsites.txt') as my_file:
-    testsite_array = my_file.readlines()
-
-
-for i in range(0, numb):
-    day.append(f.read(i))  # adds the days in the file 'days' to the variable day
-
-#day.append(f.readlines())   # adds the days in the file 'days' to the variable day
-
-print(day)  # prints the 'day' variable
-"""
-
-"""
-day = []
-with open('Days.txt') as f:
+    # THIS LINE STARTS A LOOP WHICH WILL LOOP THROUGH EVERY LINE IN THE TEXT FILE CALLED DAYS (Look to the left to
+    # see this text file)
     for line in f:
-        day.append(line)
+        # THIS LINE WILL SPLIT EACH LINE INTO A MINI LIST OF ITEMS AND STORE IT INTO THE LIST CALLED
+        # ''NUMBER_STRINGS''. THE LINE WILL BE SPLIT WHEREVER THERE IS A 'WHITE SPACE'. THIS BASICALLY MEANS WHEREVER
+        # THERE IS A SPACE ON THE LINE, THE LINE WILL BE SPLIT. SO FOR EXAMPLE, ON THE FIRST LINE WE HAVE ''MONDAY
+        # 1'', SO THE LINE WILL BE SPLIT INTO 2 ITEMS; 'MONDAY' and '1' BECAUSE THERE IS A SPACE BETWEEN MONDAY AND 1
+        # AND THE SPLIT() FUNCTIONS SPLITS THE STRING WHEREVER THERE IS A SPACE.
+        number_strings = line.split()
+
+        # NOW, WE CAN ADD THE MINI-LIST ABOVE (NUMBER_STRINGS) TO THE MAIN LIST CALLED 'DAY' AND THEN AS THIS LOOP
+        # GOES ROUND FOR EACH LINE IN THE TEXT FILE, IT WILL KEEP ADDING MINI LISTS TO THE MAIN LIST CALLED DAY AND
+        # THEN YOU END UP WITH A 2D LIST OR 2D ARRAY.
+        day.append(number_strings)  # Add the "row" to your
 
 print(day)
+'''
 
-"""
-
-"""
-with open('Days.txt') as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content]
-print(content)
-"""
-"""
-with open('Days.txt', 'r') as f:
-    array = [[str(n) for n in line.split()] for line in f]
-print(array)
-"""
-
+'''
 with open('Days.txt', 'r') as f:
     day = []
+
     for line in f:
-        number_strings = line.split()  # Split the line on runs of whitespace
-        numbers = [str(n) for n in number_strings]  # Convert to integers
-        day.append(numbers)  # Add the "row" to your list.
-    print(day)
+        number_strings = line.split()
 
-# The following line does the same thing, but in a more compact and Pythonic fashion:
-    for line in f:
-        day = [[str(n) for n in line.split()] for line in f]
-    print(day)
+        day.append(number_strings)
 
+print(day)
+'''
 
-
-with open('Days.txt', 'r') as f:
+'''
+with open('testDays.txt', 'r') as f:
     day = []
 
     for line in f:
         day = [[str(n) for n in line.split()] for line in f]
     print(day)
+'''
+
